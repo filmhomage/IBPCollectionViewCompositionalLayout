81d4407fed5b56d26377a6b986c17cd8494293bc
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2019-08-11 00:48:42 +0900

    Snapshot test

---
+++ IBPCollectionViewCompositionalLayoutTests/IBPCollectionViewCompositionalLayoutTests.swift
@@ -0,0 +1,134 @@
+import XCTest
+import SnapshotTesting
+
+class IBPCollectionViewCompositionalLayoutTests: XCTestCase {
+    func testListViewController() {
+        let viewController = ListViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+
+        showViewController(viewController: navigationController)
+        waitRunLoop()
+
+        assertSnapshot(matching: navigationController, as: .image)
+    }
+}
+
+func waitRunLoop(secs: TimeInterval = 0) {
+    RunLoop.main.run(until: Date(timeIntervalSinceNow: secs))
+}
+
+func showViewController(viewController: UIViewController) {
+    let window = UIWindow()
+    window.backgroundColor = .white
+    window.rootViewController = viewController
+    window.makeKeyAndVisible()
+}
+
+private class ListViewController: UIViewController, UICollectionViewDataSource {
+    lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+        collectionView.register(ListCell.self, forCellWithReuseIdentifier: ListCell.reuseIdentifier)
+        return collectionView
+    }()
+    lazy var collectionViewLayout: UICollectionViewLayout = {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(44))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+
+        let section = NSCollectionLayoutSection(group: group)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+
+        return layout
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 94
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListCell.reuseIdentifier, for: indexPath) as! ListCell
+        cell.label.text = "\(indexPath.section), \(indexPath.row)"
+        return cell
+    }
+}
+
+private class ListCell: UICollectionViewCell {
+    static let reuseIdentifier = "list-cell-reuse-identifier"
+    let label = UILabel()
+    let accessoryImageView = UIImageView()
+    let seperatorView = UIView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("not implemented")
+    }
+}
+
+extension ListCell {
+    func configure() {
+        seperatorView.translatesAutoresizingMaskIntoConstraints = false
+        seperatorView.backgroundColor = .lightGray
+        contentView.addSubview(seperatorView)
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        contentView.addSubview(label)
+
+        accessoryImageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(accessoryImageView)
+
+        selectedBackgroundView = UIView()
+        selectedBackgroundView?.backgroundColor = UIColor.lightGray.withAlphaComponent(0.3)
+
+        let rtl = effectiveUserInterfaceLayoutDirection == .rightToLeft
+        let chevronImageName = rtl ? "chevron.left" : "chevron.right"
+        let chevronImage = UIImage(named: chevronImageName)
+        accessoryImageView.image = chevronImage
+        accessoryImageView.tintColor = UIColor.lightGray.withAlphaComponent(0.7)
+
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
+            label.trailingAnchor.constraint(equalTo: accessoryImageView.leadingAnchor, constant: -inset),
+
+            accessoryImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            accessoryImageView.widthAnchor.constraint(equalToConstant: 13),
+            accessoryImageView.heightAnchor.constraint(equalToConstant: 20),
+            accessoryImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+
+            seperatorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            seperatorView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            seperatorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            seperatorView.heightAnchor.constraint(equalToConstant: 0.5)
+        ])
+    }
+}
